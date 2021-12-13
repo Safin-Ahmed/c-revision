446d9fcd4fc4a2e883ef66a21db3f096ab25e913
Safin-Ahmed <safin.ahmed2000@gmail.com>
2021-12-13 14:46:39 +0600

    Added Logical Operators

---
+++ logicalOperator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    // logical operators = && (AND) checks if two conditons are true.
+    float temp = 25;
+    bool sunny = 1;
+    if (temp >= 0 && temp <= 30 && sunny)
+    {
+        printf("The Weather Is Good!");
+    }
+    else
+    {
+        printf("The Weather Is Bad!");
+    }
+    return 0;
+}
